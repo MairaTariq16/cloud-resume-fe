36f5a47a76bb6f8b311bcf4263abd59e71978fd9
MairaTariq <maira.t.tariq@gmail.com>
2024-05-01 01:17:46 +0500

    feat: add certifications page

---
+++ app/certifications/page.tsx
@@ -0,0 +1,8 @@
+import Content from "@/components/content";
+import { CERTIFICATIONS } from "@/lib/constants";
+
+const CertificationsPages = () => {
+  return <Content title={CERTIFICATIONS.title} items={CERTIFICATIONS.items} />;
+};
+
+export default CertificationsPages;
