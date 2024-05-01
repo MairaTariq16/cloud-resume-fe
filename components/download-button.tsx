f6b8c6b4ebbb09cbea351d30d3efc511b0b44434
MairaTariq <maira.t.tariq@gmail.com>
2024-05-02 00:29:08 +0500

    style: toast bg

--- components/download-button.tsx
+++ components/download-button.tsx
@@ -11,11 +11,11 @@ const DownloadButton = () => {
       <Button
         onClick={() => {
           toast({
-            title: "This feature is not implemented yet",
-            variant: "default",
+            title: "This feature is not implemented yet!",
+            variant: "information",
           });
         }}
-        className="bg-primary text-secondary my-5 before:ease relative overflow-hidden border transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-primary hover:before:-translate-x-40"
+        className="bg-gr bg-primary text-secondary my-5 before:ease relative overflow-hidden border transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-primary hover:before:-translate-x-40"
       >
         Download Resume
       </Button>
